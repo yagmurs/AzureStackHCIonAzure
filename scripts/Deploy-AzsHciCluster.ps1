499193db401beb39adb056399449cceda6636515
Yagmur Sahin <yagmurs@hotmail.com>
2021-01-16 16:23:12 +0300

    update landing script

--- scripts/Deploy-AzsHciCluster.ps1
+++ scripts/Deploy-AzsHciCluster.ps1
@@ -1,8 +1,14 @@
 Import-Module AzureStackHCIInstallerHelper
 
+#run interactive
 Start-AzureStackHciSetup
 
 break
 
-#cleanup VMs 
-Start-AzureStackHciSetup -CleanupVMs 1 -
+#cleanup VMs and rerun setup unattended
+Start-AzureStackHciSetup -CleanupVMs 1 `
+    -RolesConfigurationProfile 0 `
+    -NetworkConfigurationProfile 0 `
+    -DisksConfigurationProfile 0 `
+    -ClusterConfigurationProfile 0 `
+    -AksHciConfigurationProfile 0
