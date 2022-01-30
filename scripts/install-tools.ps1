6cff0e64e5efdb1fc3e3e71678a87cbd5b4347ef
Yagmur Sahin <yagmurs@hotmail.com>
2022-01-30 22:43:35 +0300

    u

--- scripts/install-tools.ps1
+++ scripts/install-tools.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
-choco install microsoft-edge
 choco install powershell-core
+choco install windows-admin-center --params='/Port:443'
