b02f1764277cbd1ccaf51f3ce3999d0a0b65f938
Yagmur Sahin <yagmurs@hotmail.com>
2022-02-11 00:51:45 +0300

    update to poc script

--- scripts/install-tools.ps1
+++ scripts/install-tools.ps1
@@ -6,15 +6,31 @@ $natNetworkCIDR =  "192.168.0.0/16"
 $hciNodes = (Get-ADComputer -Filter { OperatingSystem -Like '*Azure Stack HCI*'} | Sort-Object)
 $subscriptionID = "4df06176-1e12-4112-b568-0fe6d209bbe2"
 $wac = $hciNodes[0]
+$newVolumeName = "volume1"
+$ws2019IsoUri = "https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
 
+################ Install managament tools for future use ################
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n=allowGlobalConfirmation
+choco install powershell-core
+choco install azure-cli
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name az -Verbose
+
+################ Prepare Azure Stack HCI nodes for cluster setup ################
 Invoke-Command -ComputerName $hciNodes.name -ScriptBlock {
+
+    # Preparing Azure Stack HCI node for Azure Arc integration (Disabling WindowsAzureGuestAgent) https://docs.microsoft.com/en-us/azure/azure-arc/servers/plan-evaluate-on-azure-virtual-machine
+    Set-Service WindowsAzureGuestAgent -StartupType Disabled -Verbose
+    Stop-Service WindowsAzureGuestAgent -Force -Verbose
+    New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254
 
     $managementNetadapterName = $using:managementNetadapterName
     $smbNetadapterName = $using:smbNetadapterName
     $vmSwitchName = $using:vmSwitchName
     $natNetworkCIDR = $using:natNetworkCIDR
 
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     choco feature enable -n=allowGlobalConfirmation
     choco install powershell-core
     choco install azure-cli
@@ -49,30 +65,44 @@ Invoke-Command -ComputerName $hciNodes.name -ScriptBlock {
 
 }
 
-################ Enable AD kerberos delegation for Windows Admin Center Computer ################
+################ Create New Azure Stack HCI cluster ################
+New-Cluster -Name $clusterName -Node $hciNodes.name -NoStorage -ManagementPointNetworkType Distributed -Verbose
 
-$gatewayObject = Get-ADComputer -Identity $wac
-$hciNodes | Set-ADComputer -PrincipalsAllowedToDelegateToAccount $gatewayObject -Verbose
+################ Enable Storage Spaces Direct on Azure Stack HCI Cluster ################
+Enable-ClusterS2D -CimSession $clusterName -Confirm:$false -Verbose
 
-Install-Module -Name Az.StackHCI
-Register-AzStackHCI -SubscriptionId $subscriptionID -ComputerName $hciNodes[0] -ResourceGroupName sil
+################ Create new thin volume on Storage Spaces Direct on Azure Stack HCI Cluster ################
+New-Volume -FriendlyName $newVolumeName -FileSystem CSVFS_ReFS -StoragePoolFriendlyName S2D* -Size 1TB -ProvisioningType Thin -CimSession $clusterName
 
-################ Install Windows Admin Center to first Azure Stack HCI Node ################
+################ Install Windows Admin Center onto first Azure Stack HCI Node ################
 Invoke-Command -ComputerName $hciNodes[0].name -ScriptBlock {
     choco install windows-admin-center --params='/Port:443'
 }
 
-New-Cluster -Name $clusterName -Node $hciNodes.name -NoStorage -verbose
+################ Enable AD kerberos delegation for Windows Admin Center Computer Account for SSO ################
+$gatewayObject = Get-ADComputer -Identity $wac
+$hciNodes | Set-ADComputer -PrincipalsAllowedToDelegateToAccount $gatewayObject -Verbose
+Get-ADComputer -Identity $clusterName | Set-ADComputer -PrincipalsAllowedToDelegateToAccount $gatewayObject -Verbose
+Get-ClusterGroup -Name "Cluster Group" -Cluster $clusterName | Move-ClusterGroup
 
-Enable-ClusterS2D -CimSession $clusterName -Confirm:$false -Verbose
-New-Volume -FriendlyName "Volume1" -FileSystem CSVFS_ReFS -StoragePoolFriendlyName S2D* -Size 1TB -ProvisioningType Thin -CimSession $clusterName
+################ Download Windows Server 2019 ISO file to Cluster Storage ################
+$isoFileDestination = "\\$($hciNodes[0].name)\c$\ClusterStorage\$newVolumeName\iso"
+New-Item -Path $isoFileDestination -ItemType Directory -Force
+Start-BitsTransfer -Source $ws2019IsoUri -Destination "$isoFileDestination\ws2019.iso"
 
 ################ Setup Intent based network config from first Azure Stack HCI Node ################
-Invoke-Command -ComputerName $hciNodes[0].name -ScriptBlock {
-    $managementNetadapterName = $using:managementNetadapterName
-    $clusterName = $using:clusterName
-    $vmSwitchName = $using:vmSwitchName
-    Add-NetIntent -Name $vmSwitchName -Management -Compute -ClusterName $clusterName -AdapterName $managementNetadapterName
-}
+## Workaround to run NetworkATC cmdlets remotely
+Copy-Item "\\$($hciNodes[0].name)\c$\Windows\System32\WindowsPowerShell\v1.0\Modules\NetworkATC\" -Destination C:\Windows\System32\WindowsPowerShell\v1.0\Modules\NetworkATC -Recurse -Force -Verbose
+Copy-Item "\\$($hciNodes[0].name)\c$\Windows\System32\NetworkAtc.Driver.dll" -Destination "C:\Windows\System32\" -Force -Verbose
+Copy-Item "\\$($hciNodes[0].name)\c$\Windows\System32\Newtonsoft.Json.dll" -Destination "C:\Windows\System32\" -Force -Verbose
+Import-Module NetworkATC
+Add-NetIntent -Name $vmSwitchName -Management -Compute -ClusterName $clusterName -AdapterName $managementNetadapterName
 
+################ Register Windows Admin Center with Azure ################
+# https://docs.microsoft.com/en-us/azure-stack/hci/deploy/register-with-azure#prerequisites-for-cluster-registration
 
+################ Register Azure Stack HCI using Windows Admin Center ################
+# https://docs.microsoft.com/en-us/azure-stack/hci/deploy/register-with-azure#register-a-cluster-using-windows-admin-center
+
+################ Register Azure Stack HCI using Powershell ################
+# https://docs.microsoft.com/en-us/azure-stack/hci/deploy/register-with-azure#register-a-cluster-using-powershell
