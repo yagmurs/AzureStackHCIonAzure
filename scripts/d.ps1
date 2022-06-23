4e08a832a2d1b3ef0561a0e02dc9474339305681
Yagmur Sahin <yagmurs@hotmail.com>
2022-06-23 23:11:22 +0300

    f

--- scripts/d.ps1
+++ scripts/d.ps1
@@ -1,3 +1,10 @@
+login-azaccoun
+
 $cred = get-credential adm01
 $param = @{myIPforRdp = Invoke-RestMethod http://ipinfo.io/json | Select-Object -exp ip; adminusername = $cred.UserName; InstallAdminCenterOnDC = $true; AdminPassword = $cred.password}
 New-AzResourceGroupDeployment -Name ashci -ResourceGroupName sil-we2 -TemplateUri https://raw.githubusercontent.com/yagmurs/AzureStackHCIonAzure/master/linkedtemplates/azuredeploy.json -TemplateParameterObject $param
+
+
+
+Get-AzVMExtension -ResourceGroupName sil-we2 -VMName ashci-dc1 | Where-Object ExtensionType -eq DSC | Remove-AzVMExtension -Force -Verbose
+New-AzResourceGroupDeployment -Name ashci -ResourceGroupName sil-we2 -Template c:\temp\azuredeploy.json -TemplateParameterObject $param
